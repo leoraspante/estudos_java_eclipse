450e8e6525f25c832a0b8f0146955dc706c26992
Leonardo <leonardo.raspante@gmail.com>
2025-11-21 17:32:43 -0300

    Adiciona atividade de final de Matrizes

--- exercícioMatrizes/src/application/Program.java
+++ exercícioMatrizes/src/application/Program.java
@@ -41,8 +41,56 @@ public class Program {
 				System.out.printf("Valor inválido!%nDigite um número inteiro maior que zero.%n");
 			}	
 		} while(qtdColunas <=0);
+		System.out.println();
 		
 		int [][] mat = new int[qtdLinhas][qtdColunas];
+		
+		// Captura e registro dos valores.
+		System.out.println("Informe os valores:");
+		for(int i=0; i<qtdLinhas; i++) {
+			for(int j=0; j<qtdColunas; j++) {
+				mat[i][j] = sc.nextInt();
+			}
+		}
+		System.out.println();
+		
+		// Bloco responsável pela manipulação do valor de X. -- Em desenvolvimento
+		System.out.print("Informe o valor de X: ");
+		int x = sc.nextInt();
+		sc.nextLine(); // Consome quebra de linha.
+		
+		// Bloco verificando se o valor de x existe na matriz.
+		boolean temX = false;
+		for(int i=0; i<qtdLinhas; i++) {
+			for(int j=0; j<qtdColunas; j++) {
+				if(mat[i][j] == x) {
+					temX = true;
+					System.out.println();
+					System.out.printf("%dª Posição de X: %d, %d%n",i+1, i, j);
+					System.out.println();
+					
+					if(j>0) { // Verifica elemento a esquerda.
+						System.out.printf("Elemento a esquerda de X: %d%n", mat[i][j-1]);
+					}
+					if(i>0) { // Verifica elemento acima.
+						System.out.printf("Elemento acima de X: %d%n", mat[i-1][j]);
+					}
+					if(j < mat[i].length-1) { // Verifica elemento a direita.
+						System.out.printf("Elemento a direita de X: %d%n", mat[i][j+1]);
+					}
+					if(i < mat.length-1) { // Verifica elemento abaixo.
+						System.out.printf("Elemento abaixo de X: %d%n", mat[i+1][j]);
+					}
+				}
+				
+			}
+		}
+
+		if(!temX) {
+			System.out.println("Valor de X não consta na matriz.");
+			System.out.println("Fim do programa.");
+		}
+		
 		
 		
 		
